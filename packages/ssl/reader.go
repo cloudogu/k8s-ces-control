ad0c7db2b835714088a0b406df63bbe21601bfb4
Joshua Sprey <joshua.sprey@cloudogu.com>
2022-12-29 15:18:44 +0100

    Use cesappd path for certificates for compatibility with the admin dogu

--- packages/ssl/reader.go
+++ packages/ssl/reader.go
@@ -65,12 +65,12 @@ func (r manager) GetCertificateCredentials() (credentials.TransportCredentials, 
 		return nil, err
 	}
 
-	err = r.copyFromRegistryToFile(certificateKeyRegistryKey, certificateKeyFilePath)
+	err = r.copyFromRegistryToFile(certificateKeyRegistryKey, CertificateKeyFilePath)
 	if err != nil {
 		return nil, err
 	}
 
-	return credentials.NewServerTLSFromFile(certificateFilePath, certificateKeyFilePath)
+	return credentials.NewServerTLSFromFile(certificateFilePath, CertificateKeyFilePath)
 }
 
 func (r manager) hasCertificate() (bool, error) {
