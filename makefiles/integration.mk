c26708c9f159c7a9d6ca04cfa58ff1f25b838118
Joshua Sprey <joshua.sprey@cloudogu.com>
2023-01-02 14:52:05 +0100

    Fix Jenkinsfile

--- makefiles/integration.mk
+++ makefiles/integration.mk
@@ -1,11 +1,12 @@
-KUBECTL_BIN?=kubectl
+KUBECTL_BIN?=${UTILITY_BIN_PATH}/kubectl
+KUBECTL_BIN_VERSION=v1.26.0
 GRPCURL_BIN?=${UTILITY_BIN_PATH}/grpcurl
 GRPCURL_BIN_VERSION?=1.8.7
 JQ_BIN?=${UTILITY_BIN_PATH}/jq
 JQ_BIN_VERSION?=1.6
 
 .PHONY: integration-test-bash
-integration-test-bash: integration-test-bash-notice ${GRPCURL_BIN} ${JQ_BIN}## Runs integration tests by bash.
+integration-test-bash: integration-test-bash-notice ${GRPCURL_BIN} ${JQ_BIN} ${KUBECTL_BIN}## Runs integration tests by bash.
 	export GRPCURL_BIN=${GRPCURL_BIN} && export KUBECTL_BIN=${KUBECTL_BIN} && export JQ_BIN=${JQ_BIN} && ./integration-test.sh
 
 .PHONY: integration-test-bash-notice
@@ -19,5 +20,10 @@ ${GRPCURL_BIN}: ${UTILITY_BIN_PATH}
 
 ${JQ_BIN}: ${UTILITY_BIN_PATH}
 	@echo "Installing jq v${JQ_BIN_VERSION}"
-	@wget -O ${JQ_BIN} https://github.com/stedolan/jq/releases/download/jq-${JQ_BIN_VERSION}/jq-linux64 > /dev/null 2>&1
+	@wget -O ${JQ_BIN} https://github.com/stedolan/jq/releases/download/jq-${JQ_BIN_VERSION}/jq-linux64
 	@chmod +x ${JQ_BIN}
+
+${KUBECTL_BIN}: ${UTILITY_BIN_PATH}
+	@echo "Installing kubectl ${KUBECTL_BIN_VERSION}"
+	@curl -o ${KUBECTL_BIN} -LO "https://dl.k8s.io/release/${KUBECTL_BIN_VERSION}/bin/linux/amd64/kubectl"
+	@chmod +x ${KUBECTL_BIN}
