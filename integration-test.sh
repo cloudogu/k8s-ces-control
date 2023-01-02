70629119750d921fd487cc4edd5ac6648087358f
Joshua Sprey <joshua.sprey@cloudogu.com>
2023-01-02 13:52:26 +0100

    Fix Jenkinsfile

--- integration-test.sh
+++ integration-test.sh
@@ -7,15 +7,16 @@ set -o pipefail
 # To run this script a local cluster is needed.
 # It is also required to install the following dogus: ldap, postfix
 
-#KUBECTL_BIN=kubectl
-#GRPCURL_BIN=grpcurl
-GRPCURL_PORT=38341
+KUBECTL_BIN_PATH="${KUBECTL_BIN:-kubectl}"
+GRPCURL_BIN_PATH="${GRPCURL_BIN:-grpcurl}"
+GRPCURL_BIN_PATH_WITH_AUTH=
+JQ_BIN_PATH="${JQ_BIN:-jq}"
 PORT_FORWARD_PID=
 
 startPortForward() {
   echo "Starting Port-Forward on ${PORT_FORWARD_PID}..."
   GRPCURL_PORT="$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')"
-  "${KUBECTL_BIN}" port-forward service/k8s-ces-control "${GRPCURL_PORT}":50051 > /dev/null 2>&1 &
+  "${KUBECTL_BIN_PATH}" port-forward service/k8s-ces-control "${GRPCURL_PORT}":50051 >/dev/null 2>&1 &
   PORT_FORWARD_PID=$!
   sleep 2s
   echo "Started Port-Forward on ${PORT_FORWARD_PID}"
@@ -81,6 +82,29 @@ function finishIntegrationTestFile() {
 EOT
 }
 
+createServiceAccount() {
+  local k8sCesControlPodName
+  k8sCesControlPodName="$("${KUBECTL_BIN_PATH}" get pods -o name | grep k8s-ces-control)"
+
+  local saCreationOutput
+  saCreationOutput="$(kubectl exec "${k8sCesControlPodName}" -- /bin/bash -c "LOG_LEVEL=error /k8s-ces-control/k8s-ces-control service-account-create integrationtest")"
+
+  local username
+  username="$(echo "${saCreationOutput}" | grep username | sed 's|username:||g')"
+
+  local password
+  password="$(echo "${saCreationOutput}" | grep password | sed 's|password:||g')"
+
+  GRPCURL_BIN_PATH_WITH_AUTH="${GRPCURL_BIN_PATH} -rpc-header servicename:integrationtest -rpc-header authorization:${username}:${password}"
+}
+
+deleteServiceAccount() {
+  local etcdClientPodName
+  etcdClientPodName="$("${KUBECTL_BIN_PATH}" get pods -o name | grep etcd-client)"
+
+  kubectl exec "${etcdClientPodName}" -- etcdctl rm -r /config/_host/k8s-ces-control/integrationtest
+}
+
 runTests() {
   echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
   echo "Test-Suite: Dogu-Administration: Test GetAllDogus, Start, Stop, Restart"
@@ -92,7 +116,7 @@ runTests() {
 
 testDoguAdministration_GetAllDogus() {
   local getDoguList
-  getDoguList="$(${GRPCURL_BIN} -insecure localhost:"${GRPCURL_PORT}" doguAdministration.DoguAdministration.GetDoguList | jq '.dogus | map(select(.name)) | .[].name')"
+  getDoguList="$(${GRPCURL_BIN_PATH_WITH_AUTH} -insecure localhost:"${GRPCURL_PORT}" doguAdministration.DoguAdministration.GetDoguList | ${JQ_BIN_PATH} '.dogus | map(select(.name)) | .[].name')"
 
   if [[ "${getDoguList}" == *"\"ldap\""* ]]; then
     echo "Test: Check if Ldap returned: Success!"
@@ -115,10 +139,10 @@ ${getDoguList}"
   fi
 }
 testDoguAdministration_StartStopDogus() {
-  ${GRPCURL_BIN} -insecure -d '{"doguName": "postfix"}' localhost:"${GRPCURL_PORT}" doguAdministration.DoguAdministration.StopDogu > /dev/null 2>&1
+  ${GRPCURL_BIN_PATH_WITH_AUTH} -insecure -d '{"doguName": "postfix"}' localhost:"${GRPCURL_PORT}" doguAdministration.DoguAdministration.StopDogu >/dev/null 2>&1
 
   local replicas=""
-  replicas="$(${KUBECTL_BIN} get deployment/postfix -o json | jq '.spec.replicas')"
+  replicas="$(${KUBECTL_BIN_PATH} get deployment/postfix -o json | ${JQ_BIN_PATH} '.spec.replicas')"
   if [[ "${replicas}" == 0 ]]; then
     echo "Test: Postfix stopped? Success!"
     addSuccessTestCase "Dogu-Administration-StopDogu-Postfix" "k8s-ces-control successfully stopped the Postfix dogu."
@@ -128,7 +152,7 @@ testDoguAdministration_StartStopDogus() {
   fi
 
   local replicas=""
-  replicas="$(${KUBECTL_BIN} get deployment/postfix -o json | jq '.spec.replicas')"
+  replicas="$(${KUBECTL_BIN_PATH} get deployment/postfix -o json | ${JQ_BIN_PATH} '.spec.replicas')"
   if [[ "${replicas}" == 0 ]]; then
     echo "Test: Postfix started? Success!"
     addSuccessTestCase "Dogu-Administration-StartDogu-Postfix" "k8s-ces-control successfully started the Postfix dogu."
@@ -139,12 +163,12 @@ testDoguAdministration_StartStopDogus() {
 }
 testDoguAdministration_RestartDogus() {
   local postfixBeforePodName=""
-  postfixBeforePodName="$(${KUBECTL_BIN} get pods -o name | grep postfix)"
+  postfixBeforePodName="$(${KUBECTL_BIN_PATH} get pods -o name | grep postfix)"
 
-  ${GRPCURL_BIN} -insecure -d '{"doguName": "postfix"}' localhost:"${GRPCURL_PORT}" doguAdministration.DoguAdministration.RestartDogu > /dev/null 2>&1
+  ${GRPCURL_BIN_PATH_WITH_AUTH} -insecure -d '{"doguName": "postfix"}' localhost:"${GRPCURL_PORT}" doguAdministration.DoguAdministration.RestartDogu >/dev/null 2>&1
 
   local postfixAfterPodName=""
-  postfixAfterPodName="$(${KUBECTL_BIN} get pods -o name | grep postfix)"
+  postfixAfterPodName="$(${KUBECTL_BIN_PATH} get pods -o name | grep postfix)"
 
   if [[ "${postfixAfterPodName}" != "${postfixBeforePodName}" ]]; then
     echo "Test: Postfix restarted? Success!"
@@ -157,11 +181,14 @@ Name after killing the pod: ${postfixAfterPodName}"
   fi
 }
 
-echo "Using KUBECTL_BIN=${KUBECTL_BIN}"
-echo "Using GRPCURL_BIN=${GRPCURL_BIN}"
+echo "Using KUBECTL=${KUBECTL_BIN_PATH}"
+echo "Using GRPCURL=${GRPCURL_BIN_PATH}"
+echo "Using JQ=${JQ_BIN_PATH}"
 
 createIntegrationTestFile
 startPortForward
+createServiceAccount
 runTests
+deleteServiceAccount
 killPortForward
 finishIntegrationTestFile
--- makefiles/integration.mk
+++ makefiles/integration.mk
@@ -1,9 +1,11 @@
 KUBECTL_BIN?=kubectl
 GRPCURL_BIN?=${UTILITY_BIN_PATH}/grpcurl
 GRPCURL_BIN_VERSION?=1.8.7
+JQ_BIN?=${UTILITY_BIN_PATH}/jq
+JQ_BIN_VERSION?=1.6
 
 .PHONY: integration-test-bash
-integration-test-bash: integration-test-bash-notice ${GRPCURL_BIN} ## Runs integration tests by bash.
+integration-test-bash: integration-test-bash-notice ${GRPCURL_BIN} ${JQ_BIN}## Runs integration tests by bash.
 	export GRPCURL_BIN=${GRPCURL_BIN} && export KUBECTL_BIN=${KUBECTL_BIN} && ./integration-test.sh
 
 .PHONY: integration-test-bash-notice
@@ -12,5 +14,9 @@ integration-test-bash-notice:
 	@echo "The kubectl binary can be overwritten with KUBECTL_BIN."
 
 ${GRPCURL_BIN}: ${UTILITY_BIN_PATH}
-	@echo "Installing grpcurl"
+	@echo "Installing grpcurl v${GRPCURL_BIN_VERSION}"
 	$(call go-get-tool,$(GRPCURL_BIN),github.com/fullstorydev/grpcurl/cmd/grpcurl@v$(GRPCURL_BIN_VERSION))
+
+${JQ_BIN}: ${UTILITY_BIN_PATH}
+	@echo "Installing jq v${JQ_BIN_VERSION}"
+	@wget -O ${JQ_BIN} https://github.com/stedolan/jq/releases/download/jq-${JQ_BIN_VERSION}/jq-linux64 > /dev/null 2>&1
